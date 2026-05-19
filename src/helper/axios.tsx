13ea453ec429ce6b16d84685b862d65cc1bd0546
tdtechsolutions2019 <tdtechsolutions2019@gmail.com>
2026-05-19 17:17:08 +0530

    Add env file

--- src/helper/axios.tsx
+++ src/helper/axios.tsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+// const BASE_URL = "http://localhost:5000/api";
 // const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const service = axios.create({
   baseURL: BASE_URL,
 });
-
 
 // 🔐 Request Interceptor (Attach Token Automatically)
 service.interceptors.request.use(
